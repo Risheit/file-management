6299017f34d8a9e325d1580357c818710d450ef3
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-09 18:22:22 -0500

    Add method to get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let input = get_input();    
+    println!("{input}");
+}
+
+fn get_input() -> String {
+    use std::io;
+    
+    print!("> ");
+    io::Write::flush(&mut io::stdout()).expect("Error when flushing");
+    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error with input reading.");
+    
+    return input;
 }
